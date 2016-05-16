5c07e1fa1422ae3bd3a2532a726aaedbb89bd3ca
Tom <tankers746@live.com>
2016-05-16 20:38:44 +0800

    Added command line options to client

--- src/client.c
+++ src/client.c
@@ -15,12 +15,12 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <math.h>
+#include <getopt.h>
 
 
 
 #define LENGTH 1024
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
-
 
 
 void error(const char *msg)
@@ -80,7 +80,7 @@ char *recvFileName(int sock) {
             //return '\0';
         }
     }
-
+    
     printf("Bytes received: %i Contents of buffer: %s\n",bytesReceived,buffer);
     strcpy(name, buffer);
     return name;
@@ -170,44 +170,44 @@ int receiveFile(int sock, char * downloadsFolder) {
         fprintf(stderr,"File %s Cannot be opened.\n", filePath);
         return EXIT_FAILURE;
     }
+    memset(buffer, '\0', LENGTH);
+    int bytesReceived = 0;
+    int totalWritten = 0;
+    int remaining = fileSize;
+    int n = 0;
+    
+    while((bytesReceived = recv(sock, buffer, MIN(LENGTH, remaining), 0)) > 0)
+    {
+        int bytesWritten = fwrite(buffer, sizeof(char), bytesReceived, fr);
+        if(bytesWritten < bytesReceived)
+        {
+            error("File write failed.\n");
+        }
         memset(buffer, '\0', LENGTH);
-        int bytesReceived = 0;
-        int totalWritten = 0;
-        int remaining = fileSize;
-        int n = 0;
+        totalWritten = totalWritten + bytesWritten;
+        remaining = remaining - bytesWritten;
+        if(round(((double)totalWritten/(double)fileSize)*100) == n) {
+            n = n+5;
+            fprintf(stderr,"|");
+        }
         
-        while((bytesReceived = recv(sock, buffer, MIN(LENGTH, remaining), 0)) > 0)
+    }
+    printf("\n");
+    if(bytesReceived < 0)
+    {
+        if (errno == EAGAIN)
         {
-            int bytesWritten = fwrite(buffer, sizeof(char), bytesReceived, fr);
-            if(bytesWritten < bytesReceived)
-            {
-                error("File write failed.\n");
-            }
-            memset(buffer, '\0', LENGTH);
-            totalWritten = totalWritten + bytesWritten;
-            remaining = remaining - bytesWritten;
-            if(round(((double)totalWritten/(double)fileSize)*100) == n) {
-                n = n+5;
-                fprintf(stderr,"|");
-            }
-            
+            printf("recv() timed out.\n");
         }
-        printf("\n");
-        if(bytesReceived < 0)
+        else
         {
-            if (errno == EAGAIN)
-            {
-                printf("recv() timed out.\n");
-            }
-            else
-            {
-                fprintf(stderr, "recv() failed due to errno = %d\n", errno);
-            }
-            
+            fprintf(stderr, "recv() failed due to errno = %d\n", errno);
         }
-        printf("Ok received from server!\n");
-        fclose(fr);
-        return EXIT_SUCCESS;
+        
+    }
+    printf("Ok received from server!\n");
+    fclose(fr);
+    return EXIT_SUCCESS;
 }
 
 int connectServer(char * serverAddr, int serverPort) {
@@ -242,17 +242,82 @@ int connectServer(char * serverAddr, int serverPort) {
     
 }
 
+void print_usage() {
+    printf("Usage:\n");
+    printf("-a filename              [add or replace a file on the oldtrusty server]\n");
+    printf("-c number                [provide the required circumference (length) of a circle of trust]\n");
+    printf("-f filename              [fetch an existing file from the oldtrusty server]\n");
+    printf("-h hostname:port         [provide the remote address hosting the server]\n");
+    printf("-l                       [list all stored files and how they are protected]\n");
+    printf("-n name                  [require a circle of trust to involve the named person]\n");
+    printf("-u certificate           [upload a certificate to the server]\n");
+    printf("-v filename certificate  [vouch for the authenticity of an existing file in the server using the indicated certificate]\n");
+}
+
+
 
 int main(int argc, char *argv[])
 {
+    
+    enum { SEND_MODE, FETCH_MODE, VOUCH_MODE } mode;
+    //------------------------
+    int option = 0;
+    int circumference = 0, port = 0;
+    char *filepath, *hostname, *trustedname, certificate;
+    
+    //Specifying the expected options
+    //The two options l and b expect numbers as argument
+    while ((option = getopt(argc, argv,"a:c:f:h:ln:u:v:")) != -1) {
+        switch (option) {
+            case 'a' :
+                filepath = optarg;
+                mode = SEND_MODE;
+                break;
+            case 'c' :
+                circumference = atoi(optarg);
+                break;
+            case 'f' :
+                filepath = optarg;
+                mode = FETCH_MODE;
+                break;
+            case 'h' :
+                hostname = strtok(optarg, ":");
+                port = atoi(strtok(NULL, ":"));
+                break;
+            case 'l' :
+                //todo create list all files method
+            case 'n' :
+                trustedname = optarg;
+            case 'u' :
+                filepath = optarg;
+                mode = SEND_MODE;
+                break;
+            case 'v' :
+                //todo create method to vouch
+                mode = VOUCH_MODE;
+                break;
+            default: print_usage();
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("filepath is %s\n", filepath);
+    printf("hostname is %s\n", hostname);
+    printf("ports is %i\n", port);
+    printf("mode is %i\n", mode);
+    
+    //---------------------
+   
+    
+    
     char *downloads = "/Users/tom/downloads";
     char *address = "192.168.15.108";
     char *path = "/Users/tom/desktop/audio-vga.m4v";
-    
-    int port = 1342;
-    /* Variable Definition */
-    int sockfd = connectServer(address, port);
 
+    /* Variable Definition */
+    /*
+    int sockfd = connectServer(address, port);
+    
     if(sendFile(sockfd,path) == EXIT_FAILURE) {
         fprintf(stderr, "Failed to send file to server\n");
     }
@@ -263,6 +328,8 @@ int main(int argc, char *argv[])
     
     close(sockfd);
     printf("[Client] Connection lost.\n");
+    */
     exit(EXIT_SUCCESS);
 }
+
 
