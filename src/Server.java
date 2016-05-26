d9074dec0fdc3502647eac7c887905c0e7ae7684
Waleed Zafar <waleedzaf94@gmail.com>
2016-05-26 18:56:26 +0800

    Add file to list

--- src/Server.java
+++ src/Server.java
@@ -18,6 +18,8 @@ public class Server extends Thread {
     public static final int BUFFSIZE = 1024;
     public final static String DOWNLOADS = "C:/Users/Waleed/Documents";
     private FileTable fileTable;
+    private int circumference = 0; //Required circumference for current client
+    private String person = null; //Possible requirement for person in circle
 
         
 	private ServerSocket ss;
@@ -32,15 +34,15 @@ public class Server extends Thread {
 	}
 	
 	public void run() {
-//        createPath("file1.txt");
+//		createPath("file1.txt");
 		while (true) {
-            System.out.println("Waiting for connection...");
+			System.out.println("Waiting for connection...");
 			try {
 				Socket clientSock = ss.accept();
-                System.out.println("Connected to client on " + clientSock.getRemoteSocketAddress() + ':' + clientSock.getLocalPort());
-                getCommand(clientSock);
+				System.out.println("Connected to client on " + clientSock.getRemoteSocketAddress() + ':' + clientSock.getLocalPort());
+				getCommand(clientSock);
 //				saveFile(clientSock);
-//                sendFile(clientSock,"C:/Users/Tom/Desktop/audio-vga (6).m4v");
+//              sendFile(clientSock,"C:/Users/Tom/Desktop/audio-vga (6).m4v");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -89,6 +91,10 @@ public class Server extends Thread {
 		return "";
 	}
 	
+	private void addFileToList(String filename){
+		this.fileTable.addFile(filename);
+	}
+	
 	private void setCertificateForFile(Socket clientSock, String filename){
 		
 	}
@@ -117,88 +123,88 @@ public class Server extends Thread {
 		DataInputStream dis = new DataInputStream(clientSock.getInputStream());
 		byte[] buffer = new byte[BUFFSIZE];
 
-		
-                int filesize =  0;
-                filesize = dis.readInt();
-                System.out.println("Filesize is " + filesize);
-                dis.read(buffer, 0, BUFFSIZE);
-                String fileName = new String(buffer).split("\0")[0];
-                System.out.println("Filename is " + fileName);
-
+        int filesize =  0;
+        filesize = dis.readInt();
+        System.out.println("Filesize is " + filesize);
+        dis.read(buffer, 0, BUFFSIZE);
+        String fileName = new String(buffer).split("\0")[0];
+        System.out.println("Filename is " + fileName);
                 
-                int read = 0;
+        int read = 0;
 		int totalRead = 0;
 		int remaining = filesize;
                 
-                File receivedFile = new File(DOWNLOADS + '/' + fileName);
+        File receivedFile = new File(DOWNLOADS + '/' + fileName);
 		FileOutputStream fos = new FileOutputStream(receivedFile);                
-                //String progress = "[                    ]";
-                int n = 0;
+        //String progress = "[                    ]";
+        int n = 0;
 		while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
 			totalRead += read;
 			remaining -= read;
 			//System.out.println("read " + totalRead + " bytes.");
 			fos.write(buffer, 0, read);
                         
-                        //System.out.println(round(((float)totalRead/(float)filesize)*100));
-                        if(round(((float)totalRead/(float)filesize)*100) == n) {
-                            n = n+5;
-                            System.out.print('|');
-                        }
+            //System.out.println(round(((float)totalRead/(float)filesize)*100));
+            if(round(((float)totalRead/(float)filesize)*100) == n) {
+                n = n+5;
+                System.out.print('|');
+            }
                         
 		}
-                System.out.println("\nFile saved in " + DOWNLOADS + '/' + fileName);
+        System.out.println("\nFile saved in " + DOWNLOADS + '/' + fileName);
 		
+        addFileToList(fileName); //Add newly saved file to list
+        
 		fos.close();
 		//dis.close();
 	}
+    
+    public byte[] intToByteArray(int value) {
+        return new byte[] {
+            (byte)(value >>> 24),
+            (byte)(value >>> 16),
+            (byte)(value >>> 8),
+            (byte)value};
+    }
+    
+    private void sendFile(Socket clientSock, String path) throws IOException {
+        File myFile = new File(path);  
+        byte[] mybytearray = new byte[(int) myFile.length()];  
+
+        FileInputStream fis = new FileInputStream(myFile);  
+        BufferedInputStream bis = new BufferedInputStream(fis);  
+        //bis.read(mybytearray, 0, mybytearray.length);  
+
+        DataInputStream dis = new DataInputStream(bis);     
+        dis.readFully(mybytearray, 0, mybytearray.length);  
+
+
+        //Sending file name and file size to the server  
+        DataOutputStream dos = new DataOutputStream(clientSock.getOutputStream());     
+        int fileSize = mybytearray.length;
+        dos.write(intToByteArray(fileSize), 0, 4);
         
-        public byte[] intToByteArray(int value) {
-            return new byte[] {
-                (byte)(value >>> 24),
-                (byte)(value >>> 16),
-                (byte)(value >>> 8),
-                (byte)value};
-        }
+        String fileName = myFile.getName() + new String(new char[BUFFSIZE]);
         
-        private void sendFile(Socket clientSock, String path) throws IOException {
-                File myFile = new File(path);  
-                byte[] mybytearray = new byte[(int) myFile.length()];  
-
-                FileInputStream fis = new FileInputStream(myFile);  
-                BufferedInputStream bis = new BufferedInputStream(fis);  
-                //bis.read(mybytearray, 0, mybytearray.length);  
-
-                DataInputStream dis = new DataInputStream(bis);     
-                dis.readFully(mybytearray, 0, mybytearray.length);  
-
-
-                //Sending file name and file size to the server  
-                DataOutputStream dos = new DataOutputStream(clientSock.getOutputStream());     
-                int fileSize = mybytearray.length;
-                dos.write(intToByteArray(fileSize), 0, 4);
-                
-                String fileName = myFile.getName() + new String(new char[BUFFSIZE]);
+        
+        System.out.println(fileName + " is "+ fileSize + " long");
+        dos.write(fileName.getBytes(), 0, BUFFSIZE);     
+        
+        dos.write(mybytearray, 0, mybytearray.length);     
+        dos.flush();  
+        System.out.println("File sent!\n");
                 
                 
-                System.out.println(fileName + " is "+ fileSize + " long");
-                dos.write(fileName.getBytes(), 0, BUFFSIZE);     
-                
-                dos.write(mybytearray, 0, mybytearray.length);     
-                dos.flush();  
-                System.out.println("File sent!\n");
-                        
-                        
 
-                //Sending file data to the server  
-//                os.write(mybytearray, 0, mybytearray.length);  
-  //              os.flush();  
+//        Sending file data to the server  
+//        os.write(mybytearray, 0, mybytearray.length);  
+//        os.flush();  
 
-                //Closing socket
-                //os.close();
-                dis.close();
-                dos.close();           
-        }
+        //Closing socket
+        //os.close();
+        dis.close();
+        dos.close();           
+    }
 	
 	public static void main(String[] args) {
 		Server fs = new Server(1342);
