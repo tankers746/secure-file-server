e266cf7e0529bf796513b3c7f3498ac5c0e03d68
Waleed Zafar <waleedzaf94@gmail.com>
2016-05-29 05:51:13 +0800

    Add FileTable

---
+++ src/FileTable.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class FileTable {
+
+	Hashtable<String, ArrayList<String>> table = new Hashtable<>();
+	
+	public void addFile(String filename){
+		table.put(filename,  new ArrayList<String>());
+	}
+	
+	public void addCertificate(String filename, String certificate){
+		if (table.containsKey(filename) == false){
+			System.err.println("Filename not found. Can't add certificate!");
+		}
+		else{
+			ArrayList<String> currList = (ArrayList<String>) table.get(filename);
+			currList.add(certificate);
+			table.put(filename, currList);
+		}
+	}
+	
+	public ArrayList<String> getList(String filename){
+		if (table.containsKey(filename)){
+			return (ArrayList<String>) table.get(filename);
+		}
+		else{
+			return null; //return null if filename doesn't exist in the table
+		}
+	}
+	
+}
