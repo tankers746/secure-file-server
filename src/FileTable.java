01901ff0bc1d9d77bfb4ad1b4f4e10c81130036c
tankers746 <tankers746@live.com>
2016-05-29 22:52:58 +0800

    Fixed Serializable problem

--- src/FileTable.java
+++ src/FileTable.java
@@ -1,22 +1,26 @@
-
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
-public class FileTable {
+public class FileTable implements Serializable{
 
+	private static final long serialVersionUID = 1L;
 	Hashtable<String, ArrayList<String>> table;
 	ArrayList<String> fileList;
 	
 	public FileTable() {
-            table = new Hashtable<>();
-            fileList = new ArrayList<>();
-        }
+        table = new Hashtable<>();
+        fileList = new ArrayList<>();
+    }
         
-        public void addFile(String filename){
-		table.put(filename,  new ArrayList<String>());
+    public void addFile(String filename){
+    	table.put(filename,  new ArrayList<String>());
 		if (!(fileList.contains(filename))){
 			fileList.add(filename);
 		}
+		saveFileTable();
 	}
 	
 	public void addCertificate(String filename, String certificate){
@@ -26,8 +30,8 @@ public class FileTable {
                 else {
 			ArrayList<String> currList = (ArrayList<String>) table.get(filename);
                         if(!currList.contains(certificate)) {
-                            currList.add(certificate);
-                            table.put(filename, currList);
+			currList.add(certificate);
+			table.put(filename, currList);
                         }
 		}
 	}
@@ -43,6 +47,17 @@ public class FileTable {
 	
 	public ArrayList<String> getFileList(){
 		return this.fileList;
+	}
+	
+	public void saveFileTable(){
+		try{
+			FileOutputStream fs = new FileOutputStream("FileTable.ser");
+			ObjectOutputStream os = new ObjectOutputStream(fs);
+			os.writeObject(this);
+			os.close();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 }
